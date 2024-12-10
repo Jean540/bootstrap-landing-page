c5932090a5ef7a69b2b73989bef37cb039152f89
Jean <jean.martins.a@gmail.com>
2024-12-10 17:10:36 -0300

    finishing header mobile menu

--- src/app/components/(navBar)/navBar.tsx
+++ src/app/components/(navBar)/navBar.tsx
@@ -1,6 +1,50 @@
+import { useEffect, useState } from "react";
 import "./navBar.css";
 
+type NavLink = {
+  title: string;
+  path: string;
+};
+
 export const NavBar = () => {
+  const [navBarOpen, setNavBarOpen] = useState(false);
+  const [size, setSize] = useState<number>(
+    typeof window !== "undefined" ? window.innerWidth : 0
+  );
+
+  useEffect(() => {
+    //  este if serve para evitar o seguinte erro: "window is not defined" que geralmente ocorre quando você está tentando acessar o objeto window em um ambiente de servidor, como Node.js
+    if (typeof window !== "undefined") {
+      const onResize = () => setSize(window.innerWidth);
+
+      window.addEventListener("resize", onResize);
+
+      return () => {
+        window.removeEventListener("resize", onResize);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    if (size >= 991) {
+      setNavBarOpen(false);
+    }
+  }, [size]);
+
+  const navLinks: NavLink[] = [
+    { title: "Home", path: "#hero" },
+    { title: "About", path: "#about" },
+    { title: "Services", path: "#services" },
+    { title: "Portfolio", path: "#portfolio" },
+    { title: "Review", path: "#reviews" },
+    { title: "Team", path: "#team" },
+    { title: "Contact", path: "#contact" },
+    { title: "Blog", path: "#blog" },
+    { title: "Download", path: "#" },
+  ];
+
+  useEffect(() => {}, []);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white sticky-top">
       <div className="container">
@@ -10,79 +54,46 @@ export const NavBar = () => {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          onClick={() => setNavBarOpen(!navBarOpen)}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#hero">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#about">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#services">
-                Services
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#portfolio">
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#reviews">
-                review
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#team">
-                Team
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#contact">
-                Contact
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#blog">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a href="#" className="btn btn-brand ms-lg-3">
-                Download
-              </a>
-            </li>
-
-            <div
-              className="dropdown-menu"
-              aria-labelledby="navbarDropdownMenuLink"
-            >
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
+            {navLinks.map((link, key) => (
+              <li key={key} className="nav-item">
+                <a
+                  className={
+                    link.title != "Download"
+                      ? "nav-link"
+                      : "btn btn-brand ms-lg-3"
+                  }
+                  href={link.path}
+                >
+                  {link.title}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
+      {navBarOpen && (
+        <div className="container menu ">
+          <ul className="navbar-nav">
+            {navLinks.map((link, key) => (
+              <li
+                key={key}
+                className="nav-item"
+                onClick={() => setNavBarOpen(false)}
+              >
+                <a className="nav-link" href={link.path}>
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
